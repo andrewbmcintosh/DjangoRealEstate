2d65a31878e42d45bd3cd184e5dd3ec70c3a92e7
andrewmcintosh <abmcintosh7@gmail.com>
2019-04-17 19:00:11 -0400

    done with listing model now starting realtor model

--- listings/models.py
+++ listings/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
 
 class Listing(models.Model):
@@ -23,3 +25,7 @@ class Listing(models.Model):
     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
