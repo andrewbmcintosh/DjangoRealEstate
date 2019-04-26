7305615db96f3834116caedc771300fa0e91599a
andrewmcintosh <abmcintosh7@gmail.com>
2019-04-26 10:16:07 -0400

    working on single listing view

--- pages/views.py
+++ pages/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
-    listings = Listing.order_by('-list_date').filter(is_published=True)[:3]
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:3]
 
     context = {
         'listings': listings
@@ -14,4 +16,15 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
